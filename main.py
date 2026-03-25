3dc8d7c4340725402794aed26b2ad94a91bc0661
Kevin Hofer <kevin.hofer@widas.de>
2026-03-25 10:39:31 +0100

    docs(): add requirements.txt

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
-import os
-from models.sn_object import SuperNovaDiaObject
-from api.lasair_client import LasairClient
-import dotenv
+from api.mongo_client import LSSTMongoClient
+from plotting import object_viewer
 
-dotenv.load_dotenv()
+M = LSSTMongoClient(uri="mongodb://localhost:27017", db_name="lsst")
+sn_objects = M.get_all()
 
-token = os.environ.get('LASAIR_API_TOKEN')
-client = LasairClient(token=token)
-# sn_ids = client.query(selected='sherlock_classifications.diaObjectId', tables='sherlock_classifications', conditions='classification LIKE "SN"')
-result = client.get_object(object_id="170019695992242795", lasair_added=True)
-print("no timeout")
-sn_obj = SuperNovaDiaObject.from_dict(result)
-# plotter.plot_object(sn_obj)
+max_days = 0
+max_sn_obj = None
+for sn_obj in sn_objects:
+    sources = [s for s in sn_obj.sources if not s.isForced]
+    forced_sources = [s for s in sn_obj.sources if s.isForced]
+
+    mjd_days = {int(s.midpointMjdTai) for s in forced_sources}
+    if len(mjd_days) > max_days:
+        max_days = len(mjd_days)
+        max_sn_obj = sn_obj
+
+print(f"Object with the most days of observations: {max_sn_obj.diaObjectId} with {max_days} days.")
+object_viewer.plot_light_curve(max_sn_obj)
+
